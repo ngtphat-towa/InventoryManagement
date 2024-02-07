6efe31029f7adac36a0f16b4a9d5b6b2f8d864ae
Phat Nguyen <89678375+ngtphat-towa@users.noreply.github.com>
2024-02-07 09:04:49 +0700

    Init project

---
+++ InventoryManagement.DesktopClient/MainForm.cs
@@ -0,0 +1,10 @@
+namespace InventoryManagement.DesktopClient
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
